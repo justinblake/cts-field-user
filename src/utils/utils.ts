de421be0a94e9502596bc8c347322ec7f759cd1c
Justin Blake <justin.blake.dev@gmail.com>
2017-12-01 14:37:09 -0700

    Employee Information and app information 12/1 #9

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,7 +11,7 @@ export class Utils {
     modal: Modal;
     loadingIsPresent: boolean = false;
     homePage: number = 0;
-    appVersion: string = '1.3.106';
+    appVersion: string = '1.3.107';
 
     constructor(public loadingCtrl: LoadingController,
                 public alertCtrl: AlertController,
