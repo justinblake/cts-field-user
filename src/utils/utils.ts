eb205eb74896785bec98656424b92ca01b3fe260
Justin Blake <justin.blake.dev@gmail.com>
2017-12-05 15:39:09 -0700

    Project Details

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,7 +11,7 @@ export class Utils {
     modal: Modal;
     loadingIsPresent: boolean = false;
     homePage: number = 0;
-    appVersion: string = '1.3.114';
+    appVersion: string = '1.3.115';
 
     constructor(public loadingCtrl: LoadingController,
                 public alertCtrl: AlertController,
