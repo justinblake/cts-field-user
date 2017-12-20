fb061ef73eec3767b1eee0628b4a27e51fb4f8d4
Justin Blake <justin.blake.dev@gmail.com>
2017-12-20 13:30:03 -0700

    Update #7

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,7 +11,7 @@ export class Utils {
     modal: Modal;
     loadingIsPresent: boolean = false;
     homePage: number = 0;
-    appVersion: string = '1.3.123';
+    appVersion: string = '1.3.124';
 
     constructor(public loadingCtrl: LoadingController,
                 public alertCtrl: AlertController,
