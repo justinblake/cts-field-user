48db9b0e11a686a333bbe57b8ba50c88f50acde0
Justin Blake <justin.blake.dev@gmail.com>
2018-01-10 16:10:04 -0700

    Update #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -64,7 +64,7 @@ import {Utils} from '../utils/utils';
 import {Pro} from '@ionic/pro';
 
 const IonicPro = Pro.init('379d0062', {
-    appVersion: "1.3.151"
+    appVersion: "1.3.153"
 });
 
 @Injectable()
