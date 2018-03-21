3f4291efb8378461cc9983efe8236aa26ec08759
Justin Blake <justin.blake.dev@gmail.com>
2018-03-21 15:49:25 -0600

    Add accuracy to managed tasks page and increment build version.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,7 +66,7 @@ import {Utils} from '../utils/utils';
 import {Pro} from '@ionic/pro';
 
 const IonicPro = Pro.init('379d0062', {
-    appVersion: "1.4.17"
+    appVersion: "1.4.18"
 });
 
 @Injectable()
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,7 +12,7 @@ export class Utils {
     modal: Modal;
     loadingIsPresent: boolean = false;
     homePage: number = 0;
-    appVersion: string = '1.4.17';
+    appVersion: string = '1.4.18';
 
     constructor(public loadingCtrl: LoadingController,
                 public alertCtrl: AlertController,
